b7d23d4df5cc1c42657af871cb44682f729781e2
476743842@qq.com <476743842@qq.com>
2018-04-13 21:38:54 +0800

    添加mesconfig配置

---
+++ src/configs/mesconfig.js
@@ -0,0 +1,81 @@
+export default {
+    msgconfig : [
+        {
+            backcolor:"",
+            title: "小浦",
+            label:"在哪吃饭？",
+            img:"http://192.168.1.88:8080/img/head.jpg",
+            icon:"",
+            fontsize:"",
+            count:5,
+            rowid:"1",
+            rename:'list1'
+        },
+        { 
+            backcolor:"",
+            title: "腾讯新闻",
+            label:"北大留美硕士控诉父母“控制与伤害” 12年未回家过年",
+            icon:"",
+            img:"http://192.168.1.88:8080/img/tenxunnew.png",
+            fontsize:"",
+            count:1,
+            rowid:"2",
+            rename:'list2'
+        },
+        {
+            backcolor:"",
+            title: "小明",
+            label:"发个链接给我",
+            img:"http://192.168.1.88:8080/img/head1.jpg",
+            icon:"",
+            fontsize:"",
+            count:"",
+            rowid:"3",
+            rename:'list3'
+        },
+        {
+            backcolor:"#18B6F7",
+            title: "通知公告",
+            label:"今年年会时间地点通知",
+            img:"",
+            icon:"\ue6b9",
+            fontsize:"65",
+            count:1,
+            rowid:"4",
+            rename:'list4'
+        },
+        {
+            backcolor:"#FFA25A",
+            title: "日报动态",
+            label:"今天去有个好地方出差",
+            img:"",
+            icon:"\ue602",
+            fontsize:"55",
+            count:8,
+            rowid:"5",
+            rename:'list5'
+        },
+        {
+            backcolor:"",
+            title: "小杰",
+            label:"明天去玩吗?",
+            img:"http://192.168.1.88:8080/img/myhead.gif",
+            icon:"",
+            fontsize:"",
+            count:"",
+            rowid:"6",
+            rename:'list6'
+        },
+        {
+            backcolor:"#FB5C67",
+            title: "我的邮件",
+            label:"您有一封来自QQ的新邮件",
+            img:"",
+            icon:"\ue6d9",
+            fontsize:"58",
+            count:"1",
+            rowid:"7",
+            rename:'list7'
+        }
+    ]
+}
